8e2234278bdc3a7c806b4f316d8eb78e260adb73
Alex Verrico <contact@alexverrico.com>
2021-04-28 18:24:40 +1000

    main.py bug fixes, code cleanup, better comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Alex Verrico (https://alexverrico.com/) all rights reserved.
+# Copyright (C) 2021 Alex Verrico (https://alexverrico.com/) All Rights Reserved.
 # For licensing enquiries please contact Alex Verrico (https://alexverrico.com/)
 
 # #####################
@@ -43,8 +43,9 @@ confChecker.check_env(required_env_variables)
 
 # Get the location of the conf.yaml file
 conf_file = os.getenv('CONF_FILE')
+# Load the conf.yaml file and store it as conf
 with open(conf_file, 'r') as f:
-    conf = yaml.load(f, Loader=yaml.FullLoader)
+    conf = yaml.safe_load(f)
 
 # List of critical conf.yaml values
 required_conf_variables = [['main_conf', 'environment_config', 'base_directory'],
@@ -93,6 +94,15 @@ app.config['DEBUG'] = api_config['flask_debug']
 # ### BEGIN GENERAL FUNCTIONS ###
 # ###############################
 
+# Create a custom logging function
+def system_log(data):
+    # Open the file SYSTEM.txt in the configured log directory in append mode
+    with open(f"{log_directory}SYSTEM.txt", 'a') as f:
+        # Append the provided data to the file
+        f.write(f'{time()}: {str(data)}\n\n\n')
+    return
+
+
 # Create a function that will run asynchronously to write to the database
 def run_queue():
     # Start a loop that will run until killed
@@ -111,65 +121,107 @@ def run_queue():
                     cursor.execute(db_update_cmd % (operation[1], operation[3]), (operation[4], operation[2],))
                     # Commit the changes
                     _con.commit()
-                    # Delete the operation variable to avoid any chance of a memory leak
+                    # Delete the variables that we are finished with to avoid any chance of a memory leak
                     del operation
+                    del db_update_cmd
                     # Jump back to the start of the loop
                     continue
+            # Check if the job type is new_row
             elif operation[0] == 'new_row':
+                # Create a connection the database which will automatically close
                 with closing(sqlite3.connect(database_path)) as _con, closing(_con.cursor()) as cursor:
+                    # Define the SQL command to run
                     db_update_cmd = 'INSERT INTO %s VALUES (%s)'
+                    # Creat the tuple of values to insert into the new row
                     param_list = []
                     for i in range(0, len(operation[2])):
                         param_list.append('?')
                     param_str = ','.join(param_list)
+                    # Run the SQL command
                     cursor.execute(db_update_cmd % (operation[1], param_str), operation[2])
+                    # Commit the changes
                     _con.commit()
+                    # Delete the variables that we are finished with to avoid any chance of a memory leak
                     del operation
+                    del db_update_cmd
+                    del param_list
+                    del param_str
+                    # Jump back to the start of the loop
                     continue
+            # Check if the job type is update_system_value
             elif operation[0] == 'update_system_value':
+                # Create a connection the database which will automatically close
                 with closing(sqlite3.connect(database_path)) as _con, closing(_con.cursor()) as cursor:
+                    # Define the SQL command to run to get the current system value(s)
                     db_fetch_value_cmd = 'SELECT system_data FROM systems_stats WHERE id = ?'
+                    # Fetch the current value(s) from the database
                     old_values = cursor.execute(db_fetch_value_cmd, (operation[1],)).fetchone()[0]
+                    # Load the current values as a json object to make manipulation easier
                     new_values = json.loads(old_values)
+                    # Store the new value
                     new_values[operation[2]] = json.loads(operation[3])
+                    # Define the SQL command to store the new value
                     db_update_value_cmd = 'UPDATE systems_stats SET system_data = ? WHERE id = ?'
+                    # Run the SQL command to store the new values
                     cursor.execute(db_update_value_cmd, (json.dumps(new_values), operation[1],))
+                    # Commit the changes
                     _con.commit()
+                    # Delete the variables that we are finished with to avoid any chance of a memory leak
                     del operation
+                    del db_fetch_value_cmd
+                    del db_update_value_cmd
+                    del old_values
+                    del new_values
+                    # Jump back to the start of the loop
                     continue
             else:
+                # Log the unsupported operation
+                system_log(f'ERROR:000001 Unrecognised operation in DB queue: {operation}\n')
+                # Wait 0.1 seconds then skip back to the start of the loop
                 sleep(0.1)
                 continue
         else:
+            # Wait 0.1 seconds then skip back to the start of the loop
             sleep(0.1)
             continue
 
 
+# Create a function to hash a password
 def hash_password(password):
-    # Hash a password for storing
+    # Generate some salt to add to the password
     salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    # Hash the password along with the salt
     pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'), salt, 100000)
+    # Convert the binary hash to hexadecimal
     pwdhash = binascii.hexlify(pwdhash)
+    # Return the hashed password
     return (salt + pwdhash).decode('ascii')
 
 
+# Create a function to compare a plaintext password to a hashed password
 def verify_password(stored_password, provided_password):
-    # Verify a stored password against one provided by user
+    # Get the salt from the stored password
     salt = stored_password[:64]
+    # Get the hash of the stored password
     stored_password = stored_password[64:]
+    # Generate a hash for the provided_password
     pwdhash = hashlib.pbkdf2_hmac('sha512',
                                   provided_password.encode('utf-8'),
                                   salt.encode('ascii'),
                                   100000)
+    # Convert the binary hash to hexadecimal
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    # Compare the provided passwords
     return pwdhash == stored_password
 
 
-# Check if a set of credentials is valid
+# Create a function to check if a set of credentials is valid
 def auth(_id, _auth, access_level='system'):
     # Create a database connection and cursor object
     with closing(sqlite3.connect(database_path)) as db_connection, closing(db_connection.cursor()) as db_cursor:
+        # define the command for fetching the password
         _auth_cmd = 'SELECT password FROM auth WHERE id = ? AND access_level = ?'
+        # Check what access level the client is attempting to authenticate for and execute the appropriate command
         if access_level == 'system':
             _temp = db_cursor.execute(_auth_cmd, (str(_id), 'system',)).fetchone()
         elif access_level == 'client':
@@ -178,47 +230,68 @@ def auth(_id, _auth, access_level='system'):
             _temp = db_cursor.execute(_auth_cmd, (str(_id), 'admin',)).fetchone()
         elif access_level == 'owner':
             _temp = db_cursor.execute(_auth_cmd, (str(_id), 'owner',)).fetchone()
+        # This allows a client to check if they are in the database anywhere regardless of their access level
         elif access_level == 'any':
             _temp = db_cursor.execute('SELECT password FROM auth WHERE id = ?', (str(_id),)).fetchone()
+        # If the access level provided was unrecognised we log it and return False
         else:
+            system_log(f'ERROR:000002 Unrecognised access_level in auth(): {access_level}\n')
             return False
+        # Attempt to convert the stored password and provided password to
+        # strings then return the result of verify_password()
         try:
             return verify_password(str(_temp[0]), str(_auth))
+        # If converting the stored password and provided passwords to strings fails then log it and return False
         except TypeError:
+            system_log(f'WARN: Failed to convert passwords to strings')
             return False
+        return False
 
 
+# Create a function to generate a new set of credentials for the API
 def generate_new_auth():
+    # Don't use certain characters for IDs and passwords to avoid ambiguity
     char_blacklist = 'iI1lo0OBgzsS'
-    temp = f'{ascii_lowercase}{ascii_digits}'
+    # Create the list of characters to use for ID generation
     id_rand_list = ''
-    for i in temp:
+    for i in f'{ascii_lowercase}{ascii_digits}':
         if i not in char_blacklist:
             id_rand_list = id_rand_list + i
+    # Start a loop to continue generating IDs until we generate an unused one
     while True:
+        # Generate an ID
         id_out = ''
         for i in range(0, 6):
             id_out = f'{id_out}{choice(id_rand_list)}'
+        # Check if the ID is already in use
         with closing(sqlite3.connect(database_path)) as _db, closing(_db.cursor()) as _cur:
             if (id_out,) in _cur.execute('SELECT "id" FROM auth').fetchall():
                 sleep(3)
                 continue
+            # If the ID isn't used then we break out of this loop
             break
-    temp = f'{temp}{ascii_uppercase}'
+    # Create the list of characters to use for password generation
     pass_rand_list = ''
-    for i in temp:
+    for i in f'{ascii_lowercase}{ascii_digits}{ascii_uppercase}':
         if i not in char_blacklist:
             pass_rand_list = pass_rand_list + i
+    # Generate a password
     pass_out = ''
     for i in range(0, 10):
         pass_out = f'{pass_out}{choice(pass_rand_list)}'
+    # Hash the generate password
     hashed_pass_out = hash_password(pass_out)
+    # Return the generated password in plaintext and hashed form, along with the ID
     return id_out, pass_out, hashed_pass_out
 
 
+# Create a function for logging historical API data for later analysis
 def api_historical(_id, _val_name, _data):
+    # Check if we should be logging historical data
     if api_enable_historical is True:
+        # Open the file in append mode
         with open(f'{historical_directory}{_id}/{_val_name}.txt', 'a') as f:
+            # Append the value
             f.write(f'"{time()}": "{str(_data)}"\n')
     return True
 
@@ -238,13 +311,9 @@ def api_admin_new_auth():
     for arg in required_args:
         if arg not in request.values:
             return f'Error: missing required argument "{arg}"', 400
-    if request.args['access_level'] == 'system':
-        if 'system_name' not in request.args:
-            return 'Error: missing required argument "system_name"', 400
-        _system_name = request.args['system_name']
-    _id = request.args['id']
-    _auth = request.args['auth']
-    _access_level = request.args['access_level']
+    _id = request.values['id']
+    _auth = request.values['auth']
+    _access_level = request.values['access_level']
     if _access_level == 'admin':
         if auth(_id, _auth, access_level='owner') is False:
             return 'Error: you do not have the proper credentials', 401
@@ -253,8 +322,8 @@ def api_admin_new_auth():
             return 'Error: you do not have the proper credentials', 401
     new_id, new_auth, hashed_new_auth = generate_new_auth()
     database_operations_queue.put(['new_row', 'auth', (new_id, hashed_new_auth, _access_level,)])
-    if _system_name:
-        database_operations_queue.put(['new_row', 'systems_stats', (new_id, _system_name, None, '{}')])
+    if _access_level == 'system':
+        database_operations_queue.put(['new_row', 'systems_stats', (new_id, '', None, '{}')])
     if os.path.exists(f'{historical_directory}{new_id}/') is False:
         os.makedirs(f'{historical_directory}{new_id}/')
     sleep(5)
@@ -275,13 +344,13 @@ def api_admin_new_auth():
 @app.route(f'{api_base_url}{api_general_prefix}check_auth', methods=['POST', 'GET'])
 def api_check_auth(_id=None, _auth=None):
     if not _id:
-        if 'id' not in request.args:
+        if 'id' not in request.values:
             return 'Error: Missing required argument "id"', 400
-        _id = request.args['id']
+        _id = request.values['id']
     if not _auth:
-        if 'auth' not in request.args:
+        if 'auth' not in request.values:
             return 'Error: Missing required argument "auth"', 400
-        _auth = request.args['auth']
+        _auth = request.values['auth']
     if auth(_id, _auth, access_level='any') is False:
         return 'Error: you do not have the proper credentials', 401
     return '', 200
@@ -295,14 +364,14 @@ def api_check_auth(_id=None, _auth=None):
 # ### BEGIN SYSTEM ENDPOINTS ###
 # ##############################
 
-@app.route(f'{api_base_url}{api_value_update_prefix}heartbeat', methods=['GET', 'POST'])
+@app.route(f'{api_base_url}{api_value_update_prefix}heartbeat', methods=['POST'])
 def api_update_heartbeat():
     required_args = ['id', 'auth']
     for arg in required_args:
         if arg not in request.values:
             return f'Error: missing required argument "{arg}"', 400
-    _id = request.args['id']
-    _auth = request.args['auth']
+    _id = request.values['id']
+    _auth = request.values['auth']
     if auth(_id, _auth, access_level='system') is False:
         return 'Error: you do not have the proper credentials', 401
     now = time()
@@ -311,7 +380,7 @@ def api_update_heartbeat():
     return '', 200
 
 
-@app.route(f'{api_base_url}{api_value_update_prefix}logging', methods=['GET', 'POST'])
+@app.route(f'{api_base_url}{api_value_update_prefix}logging', methods=['POST'])
 def api_update_logging():
     required_args = ['id', 'auth', 'data']
     for arg in required_args:
@@ -323,11 +392,11 @@ def api_update_logging():
         return 'Error: you do not have the proper credentials', 401
     _data = request.values['data']
     with open(f"{log_directory}{str(_id)}.txt", 'a') as f:
-        f.write(f'{time()}:     {str(_data)}\n\n\n')
+        f.write(f'{time()}: {str(_data)}\n\n\n')
     return '', 200
 
 
-@app.route(f'{api_base_url}{api_value_update_prefix}main', methods=['GET', 'POST'])
+@app.route(f'{api_base_url}{api_value_update_prefix}main', methods=['POST'])
 def api_update_main():
     required_args = ['id', 'auth', 'value', 'data']
     for arg in required_args:
